cef4fffb23e15eaae0fd939181a0c3b84d4f6d8d
Merily Viks <merily.viks@gmail.com>
2023-05-16 12:15:10 +0300

    first

---
+++ lib/api/api.dart
@@ -0,0 +1,3 @@
+class APIKeys {
+  static const openWeatherAPIKey = '463d7eba8561093a975ed69dca7bd5c0';
+}
---
+++ lib/entities/forecast/forecast_data.dart
@@ -0,0 +1,13 @@
+import 'package:weatherapp/entities/forecast/forecast.dart';
+import 'package:weatherapp/entities/weather/weather_data.dart';
+
+/// Derived model class used in the UI
+class ForecastData {
+  const ForecastData(this.list);
+  factory ForecastData.from(Forecast forecast) {
+    return ForecastData(
+      forecast.list.map((item) => WeatherData.from(item)).toList(),
+    );
+  }
+  final List<WeatherData> list;
+}
